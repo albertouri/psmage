523a5a50341a2462ee22cf8fe948d243881e5787
Jordan Santell <jsantell@gmail.com>
2012-03-11 20:38:44 -0400

    Most chk format integrated..

--- converter/convert.rb
+++ converter/convert.rb
@@ -7,16 +7,28 @@
   'long'  => 'L'   # small-endian long, 4 bytes
 }
 @b_string = ''
+width = 128
+height = 128
+
 vcod = (File.open('vcod.bin', 'rb')).read
 puni = []; 5700.times { |x| puni << 1 }
 unit = []; #214 (int) is starting location
 isom = []; # (width/2 + 1) * (height+1) * 4, 16 int
-tile = []; # w*h * 2 (because ints), one int per tile
-
-width = 128
-height = 128
-
-
+tile = []; 4096.times { |x| tile << 1 } # w*h * 2 (because ints), one int per tile
+mask = []; (width*height).times { |x| mask << 255 }
+str  = []; str << 1024; 1024.times { |x| str << 0 }
+uprp = []; 1280.times { |x| uprp << 0 }
+upus = []; 64.times { |x| upus << 0 }
+mrgn = []; 5100.times { |x| mrgn << 0 }
+forc = []; 20.times { |x| forc << 0 }
+wav  = []; 1024.times { |x| wav << 0 }
+swnm = []; 256.times { |x| swnm << 0 }
+colr = []; (0..7).each { |x| colr << x }
+pupx = []; 2318.times { |x| pupx << 1 } # 1 is default for last bytes
+ptex = []; 1672.times { |x| ptex << 1 } # 1 is default for last bytes
+unix = []; 4168.times { |x| unix << 1 } # 1 is default for first bytes
+upgx = []; 794.times { |x| upgx << 1 } # 1 is default for first bytes
+tecx = []; 396.times { |x| tecx << 1 }# 1 is default for first bytes
 sections = [
   # Type of scenario file
   { 'name'=> 'TYPE', 'length'=> 4,    'val'=> 'RAWB', 'type'=> 'chars' },
@@ -40,12 +52,55 @@ sections = [
   { 'name'=> 'SIDE', 'length'=> 12,    'val'=>[1,0,2,1,0,2,1,0,7,7,7,4], 'type'=>'bytes'  },
   # Graphical tile map, only different than TILE if doodads present
   { 'name'=> 'MTXM', 'length'=>tile.length * 2, 'val'=> tile, 'type'=>'int' },
+  # UPGR (not used in other maps, not required?)
+  # PTEC (not used in other maps, not required?)
   # Player unit restrictions, all 1s for normal defaults (5700 times)
   { 'name'=> 'PUNI', 'length'=>5700,   'val'=> puni, 'type'=>'bytes' },
   # Flags and stats of each unit on the map (only starting locations for this)
   { 'name'=> 'UNIT', 'length'=>72,     'val'=> unit, 'type'=>'chars' },
   # Tile map of level's terrain 
-  { 'name'=> 'TILE', 'length'=>tile.length * 2, 'val'=> tile, 'type'=>'int' }
+  { 'name'=> 'TILE', 'length'=>tile.length * 2, 'val'=> tile, 'type'=>'int' },
+  # Doodad map of level (not using)
+  { 'name'=> 'DD2',  'length'=>0, 'val' => '', 'type'=>'chars' },
+  # Doodad to unit map
+  { 'name'=> 'THG2',  'length'=>0, 'val' => '', 'type'=>'chars' },
+  # Fog of war
+  { 'name'=> 'MASK',  'length'=>mask.length, 'val' => mask, 'type'=>'bytes' },
+  # Strings used. Zeroing out. 
+  { 'name'=> 'STR',  'length'=>str.length*2, 'val' => str, 'type'=>'int' },
+  # UPRP, triggers ???
+  { 'name'=> 'UPRP',  'length'=>uprp.length, 'val' => uprp, 'type'=>'bytes' },
+  # UPUS, trigger units, don't use
+  { 'name'=> 'UPUS',  'length'=>upus.length, 'val' => upus, 'type'=>'bytes' },
+  # MRGN Locations, docs say 256 locations, maps actually have 255
+  { 'name'=> 'MRGN',  'length'=>mrgn.length, 'val' => mrgn, 'type'=>'bytes' },
+  # TRIG
+  # 
+  # Mission briefing section
+  { 'name'=>'MBRF', 'length'=>0, 'val'=>'', 'type'=>'chars' },
+  # Connect scenario properties to strings (pointing to null strings in STR)
+  { 'name'=>'SPRP', 'length'=>4, 'val'=>[0,0], 'type'=>'int' },
+  # Force/team specs
+  { 'name'=>'FORC', 'length'=>20, 'val'=>forc, 'type'=>'bytes' },
+  # Paths to WAV files used
+  { 'name'=>'WAV',  'length'=>wav.length*2, 'val'=>wav, 'type'=>'int' },
+  # UNIS not used
+  # UPGS not used
+  # TECS not used
+  # String of switch names
+  { 'name'=>'SWNM', 'length'=>swnm.length*4, 'val'=>swnm, 'type'=>'long' },
+  # Colors per player
+  { 'name'=>'COLR', 'length'=>colr.length, 'val'=>colr, 'type'=>'bytes' },
+  # PUPx overrides UPGR for BW, setting all to 1 to let defaults take over
+  { 'name'=>'PUPx', 'length'=>pupx.length, 'val'=>pupx, 'type'=>'bytes' },
+  # PTEx overrides PTEC for BW, setting all to 1 to let defaults take over
+  { 'name'=>'PTEx', 'length'=>ptex.length, 'val'=>ptex, 'type'=>'bytes' },
+  # UNIx overrides UNIS for BW, setting all to 1 to let defaults take over
+  { 'name'=>'UNIx', 'length'=>unix.length, 'val'=>unix, 'type'=>'bytes' },
+  # UPGx overrides UPGS for BW, setting all to 1 to let defaults take over
+  { 'name'=>'UPGx', 'length'=>upgx.length, 'val'=>upgx, 'type'=>'bytes' },
+  # TECx overrides TECS for BW, setting all to 1 to let defaults take over
+  { 'name'=>'TECx', 'length'=>tecx.length, 'val'=>tecx, 'type'=>'bytes' },
 
 ]
 
@@ -53,7 +108,6 @@ def parse sections
   sections.each do |sec|
     create_section sec['name'], sec['length'], sec['val'], sec['type']
   end
-  p @b_string
   File.open('output.chk', 'wb') { |f|
     f.write @b_string
   }
